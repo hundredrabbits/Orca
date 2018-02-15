4961f84da65bf3ac30c1b4760bfa424427e38b0c
Devine Lu Linvega <aliceffekt@gmail.com>
2018-02-15 21:59:34 +1300

    Migrating to canvas

--- desktop/sources/scripts/grid.js
+++ desktop/sources/scripts/grid.js
@@ -1,6 +1,10 @@
 function Grid()
 {
-  this.el = document.createElement("grid");
+  this.el = document.createElement("canvas");
+
+  this.size = {width:600,height:600,ratio:1}
+  this.el.width = this.size.width;
+  this.el.height = this.size.height;
 
   this.cursor = {
     x:0,
@@ -24,27 +28,41 @@ function Grid()
 
   this.update = function()
   {
-    var html = "";
+    this.clear();
     var y = 0;
     while(y < pico.program.h){
       var x = 0;
       while(x < pico.program.w){
-        if(this.cursor.x == x && this.cursor.y == y){
-          html += `<c>${pico.program.glyph_at(x,y)}</c>`;
-        }
-        else if(pico.program.is_locked(x,y)){
-          html += `<l>${pico.program.glyph_at(x,y)}</l>`; 
-        }
-        else{
-          html += pico.program.glyph_at(x,y);  
-        }
+        this.draw_sprite(x,y,pico.program.glyph_at(x,y));
         x += 1
       }
-      html += "\n"
       y += 1;
     }
-    this.el.innerHTML = html;
   }
+
+  this.context = function()
+  {
+    return this.el.getContext('2d');
+  }
+
+  this.clear = function()
+  {
+    var ctx = this.context();
+
+    ctx.clearRect(0, 0, this.size.width * this.size.ratio, this.size.height * this.size.ratio);
+  }
+
+  this.draw_sprite = function(x,y,g)
+  {
+    var font_size    = 10;
+    var ctx          = this.context();
+    ctx.font         = `${font_size}px`;
+    ctx.fillStyle    = 'white';
+    ctx.textBaseline = 'top';
+    ctx.textAlign    = "left"; 
+    ctx.fillText(g, x * 10, y * 10);
+  }
+
 
   function clamp(v, min, max) { return v < min ? min : v > max ? max : v; }
 }
