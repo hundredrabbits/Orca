5a87c83e27175b8e1f26bcd79c9ff881e7e69727
Devine Lu Linvega <aliceffekt@gmail.com>
2018-06-08 08:58:46 +1200

    Fixed E func

--- desktop/sources/scripts/programs/e.js
+++ desktop/sources/scripts/programs/e.js
@@ -9,19 +9,17 @@ function program_E(x,y)
 
   this.operation = function()
   {
-    if(this.bang()){
-      this.fire()
-      this.lock()
-    }
-  }
+    var b = this.bang();
 
-  this.fire = function()
-  {
+    if(!b){ return; }
+
+    this.remove();
+    pico.program.lock(b.x,b.y);
+    
     var ns = this.free_neighbors();
     for(id in ns){
       var n = ns[id];
       pico.program.add(n.x,n.y,"b");
-      pico.program.lock(n.x,n.y);
     }
   }
 }
