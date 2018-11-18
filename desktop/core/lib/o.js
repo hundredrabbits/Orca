02b385405cd4c324ff5fd2e444e5b961b881dd1c
Devine Lu Linvega <aliceffekt@gmail.com>
2018-11-18 13:13:44 +1200

    Added Offset operator.

--- desktop/core/lib/o.js
+++ desktop/core/lib/o.js
@@ -1,9 +1,30 @@
 'use strict'
 
 const FnBase = require('./_base')
-// TODO
+
 function FnO (pico, x, y, isPassive) {
   FnBase.call(this, pico, x, y, 'o', isPassive)
+
+  this.name = 'offset'
+  this.info = 'Reads a distant fn with offset.'
+
+  this.ports.haste.x = { x: -2, y: 0 }
+  this.ports.haste.y = { x: -1, y: 0 }
+  this.ports.input.val = { x: 1, y: 0 }
+  this.ports.output = true
+
+  this.haste = function () {
+    const x = clamp(this.listen(this.ports.haste.x, true), 1, 16)
+    const y = clamp(this.listen(this.ports.haste.y, true), 0, 16)
+    this.ports.input.val = { x: x, y: y }
+  }
+
+  this.operation = function () {
+    const res = this.listen(this.ports.input.val)
+    this.output(`${res}`)
+  }
+
+  function clamp (v, min, max) { return v < min ? min : v > max ? max : v }
 }
 
 module.exports = FnO
