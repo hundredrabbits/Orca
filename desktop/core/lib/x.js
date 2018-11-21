53bdadbdcfca7e34752d943ba1e1d47cf1724e40
Devine Lu Linvega <aliceffekt@gmail.com>
2018-11-21 20:16:52 +1200

    Unlock X output

--- desktop/core/lib/x.js
+++ desktop/core/lib/x.js
@@ -11,15 +11,19 @@ function FnX (orca, x, y, passive) {
   this.ports.haste.x = { x: -2, y: 0 }
   this.ports.haste.y = { x: -1, y: 0 }
   this.ports.input.val = { x: 1, y: 0 }
-  this.ports.output = { x: 0, y: 1 }
+  this.ports.output = { x: 0, y: 1, unlock: true }
 
   this.haste = function () {
     const x = clamp(this.listen(this.ports.haste.x, true), 0, 16)
     const y = clamp(this.listen(this.ports.haste.y, true), 1, 16)
-    this.ports.output = { x: x, y: y }
+    this.ports.output = { x: x, y: y, unlock: true }
   }
 
   this.run = function () {
+    if (!this.bang()) { return }
+
+    this.draw = false
+
     const res = this.listen(this.ports.input.val)
     this.output(`${res}`)
   }
