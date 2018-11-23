00c7075fcf8d1f78c0113e8de21ed9bd57421c01
Devine Lu Linvega <aliceffekt@gmail.com>
2018-11-24 09:17:28 +1200

    Removed min param in `C`

--- desktop/core/lib/b.js
+++ desktop/core/lib/b.js
@@ -6,7 +6,7 @@ function FnB (orca, x, y, passive) {
   Fn.call(this, orca, x, y, 'b', passive)
 
   this.name = 'banger'
-  this.info = 'Bangs if input is `1`, `N`, `S`, `W`, `Z` or `E`.'
+  this.info = 'Bangs if input is `1`, `N`, `S`, `W`, `E` or `Z`.'
 
   this.ports.input.val = { x: 1, y: 0, unlock: true }
   this.ports.output = { x: 0, y: 1 }
--- desktop/core/lib/c.js
+++ desktop/core/lib/c.js
@@ -8,14 +8,12 @@ function FnC (orca, x, y, passive) {
   this.name = 'clock'
   this.info = 'Outputs a constant value based on the runtime frame.'
 
-  this.ports.input.min = { x: 1, y: 0 }
-  this.ports.input.max = { x: 2, y: 0 }
+  this.ports.input.mod = { x: 1, y: 0 }
   this.ports.output = { x: 0, y: 1 }
 
   this.run = function () {
-    const min = this.listen(this.ports.input.min, true)
-    const max = this.listen(this.ports.input.max, true)
-    const key = (orca.f % (max || 10)) + min
+    const mod = this.listen(this.ports.input.mod, true)
+    const key = (orca.f % (mod || 10))
     const res = orca.allowed[key] ? orca.allowed[key] : 0
     this.output(`${res}`)
   }
