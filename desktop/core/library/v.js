61a2cbe0f3fd6a501a0f28420f18a88a54c479a4
Devine Lu Linvega <aliceffekt@gmail.com>
2018-12-04 10:23:38 +1200

    Changed the V operator

--- desktop/core/library/v.js
+++ desktop/core/library/v.js
@@ -5,25 +5,45 @@ const Operator = require('../operator')
 function OperatorV (orca, x, y, passive) {
   Operator.call(this, orca, x, y, 'v', passive)
 
-  this.name = 'beam'
-  this.info = 'Bangs the nearest southward operator on bang.'
+  this.name = 'variable'
+  this.info = 'Reads and write globally available variables.'
 
-  this.ports.output = { x: 0, y: 1, unlock: true }
+  this.ports.haste.write = { x: -1, y: 0 }
+  this.ports.input.value = { x: 1, y: 0 }
+  this.ports.output = { x: 0, y: 1 }
 
   this.haste = function () {
-    while (orca.inBounds(this.x + this.ports.output.x, this.y + this.ports.output.y)) {
-      this.ports.output.y += 1
-      if (this.listen(this.ports.output) !== '.' && this.listen(this.ports.output) !== '*') { break }
+    const key = this.listen(this.ports.haste.write, true)
+
+    if (key > 9) {
+      this.ports.output = null
     }
-    this.ports.output.y -= 1
   }
 
   this.run = function () {
-    if (!this.bang()) { return }
+    if (this.listen(this.ports.haste.write, true) > 9) { return }
 
-    this.draw = false
+    const key = this.listen(this.ports.input.value)
+    const res = this.read(key)
 
-    this.output(`*`)
+    if (res !== '.') {
+      this.output(`${res}`)
+    }
+  }
+
+  this.read = function (key) {
+    for (const id in orca.runtime) {
+      const operator = orca.runtime[id]
+      if (orca.lockAt(operator.x, operator.y)) { continue }
+      const glyph = operator.glyph.toLowerCase()
+      if (glyph !== 'v') { continue }
+      const write = operator.listen(operator.ports.haste.write)
+      if (write !== key) { continue }
+      const value = operator.listen(operator.ports.input.value)
+      return value
+    }
+
+    return null
   }
 }
 
