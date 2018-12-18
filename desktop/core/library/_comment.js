7c3ac240b6fdf1da780e025eebcbd4ab276298dc
Devine Lu Linvega <aliceffekt@gmail.com>
2018-12-19 10:36:26 +1200

    Optimized comments

--- desktop/core/library/_comment.js
+++ desktop/core/library/_comment.js
@@ -10,20 +10,12 @@ function OperatorComment (orca, x, y, passive) {
   this.draw = false
 
   this.haste = function () {
-    let count = 0
-    for (let x = 0; x <= this.x - 1; x++) {
-      if (orca.glyphAt(x, this.y) === this.glyph) { count++ }
+    for (let x = this.x + 1; x <= orca.w; x++) {
+      orca.lock(x, this.y)
+      if (orca.glyphAt(x, this.y) === this.glyph) { break }
     }
-
     this.passive = false
     this.lock()
-
-    if (count % 2 !== 0) { return }
-
-    for (let x = this.x + 1; x <= orca.w; x++) {
-      if (orca.glyphAt(x, this.y) === this.glyph) { break }
-      orca.lock(x, this.y)
-    }
   }
 }
 
