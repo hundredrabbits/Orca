352eaf106efedf7a130e0692d48644e1f3384196
makio135 <radisson@MAC22219-6.local>
2019-01-11 10:08:01 +0100

    complete tidal.js config file for OSC

--- desktop/core/bridge/tidal.js
+++ desktop/core/bridge/tidal.js
@@ -2,6 +2,309 @@
 
 module.exports = {
   name: 'Tidal OSC',
+  protocol: 'osc',
   port: 6010,
-  protocol: 'osc'
+  remoteAddress: '127.0.0.1',
+
+  '0': {
+    address: '/ctrl',
+    name: '0',
+    type: 'f'
+  },
+  '1': {
+    address: '/ctrl',
+    name: '0',
+    type: 'f'
+  },
+  '2': {
+    address: '/ctrl',
+    name: '2',
+    type: 'f'
+  },
+  '3': {
+    address: '/ctrl',
+    name: '3',
+    type: 'f'
+  },
+  '4': {
+    address: '/ctrl',
+    name: '4',
+    type: 'f'
+  },
+  '5': {
+    address: '/ctrl',
+    name: '5',
+    type: 'f'
+  },
+  '6': {
+    address: '/ctrl',
+    name: '6',
+    type: 'f'
+  },
+  '7': {
+    address: '/ctrl',
+    name: '7',
+    type: 'f'
+  },
+  '8': {
+    address: '/ctrl',
+    name: '8',
+    type: 'f'
+  },
+  '9': {
+    address: '/ctrl',
+    name: '9',
+    type: 'f'
+  },
+  'A': {
+    address: '/ctrl',
+    name: 'A',
+    type: 'f'
+  },
+  'B': {
+    address: '/ctrl',
+    name: 'B',
+    type: 'f'
+  },
+  'C': {
+    address: '/ctrl',
+    name: 'C',
+    type: 'f'
+  },
+  'D': {
+    address: '/ctrl',
+    name: 'D',
+    type: 'f'
+  },
+  'E': {
+    address: '/ctrl',
+    name: 'E',
+    type: 'f'
+  },
+  'F': {
+    address: '/ctrl',
+    name: 'F',
+    type: 'f'
+  },
+  'G': {
+    address: '/ctrl',
+    name: 'G',
+    type: 'f'
+  },
+  'H': {
+    address: '/ctrl',
+    name: 'H',
+    type: 'f'
+  },
+  'I': {
+    address: '/ctrl',
+    name: 'I',
+    type: 'f'
+  },
+  'J': {
+    address: '/ctrl',
+    name: 'J',
+    type: 'f'
+  },
+  'K': {
+    address: '/ctrl',
+    name: 'K',
+    type: 'f'
+  },
+  'L': {
+    address: '/ctrl',
+    name: 'L',
+    type: 'f'
+  },
+  'M': {
+    address: '/ctrl',
+    name: 'M',
+    type: 'f'
+  },
+  'N': {
+    address: '/ctrl',
+    name: 'N',
+    type: 'f'
+  },
+  'O': {
+    address: '/ctrl',
+    name: 'O',
+    type: 'f'
+  },
+  'P': {
+    address: '/ctrl',
+    name: 'P',
+    type: 'f'
+  },
+  'Q': {
+    address: '/ctrl',
+    name: 'Q',
+    type: 'f'
+  },
+  'R': {
+    address: '/ctrl',
+    name: 'R',
+    type: 'f'
+  },
+  'S': {
+    address: '/ctrl',
+    name: 'S',
+    type: 'f'
+  },
+  'T': {
+    address: '/ctrl',
+    name: 'T',
+    type: 'f'
+  },
+  'U': {
+    address: '/ctrl',
+    name: 'U',
+    type: 'f'
+  },
+  'V': {
+    address: '/ctrl',
+    name: 'V',
+    type: 'f'
+  },
+  'X': {
+    address: '/ctrl',
+    name: 'X',
+    type: 'f'
+  },
+  'Y': {
+    address: '/ctrl',
+    name: 'Y',
+    type: 'f'
+  },
+  'Z': {
+    address: '/ctrl',
+    name: 'Z',
+    type: 'f'
+  },
+  'a': {
+    address: '/ctrl',
+    name: 'a',
+    type: 'f'
+  },
+  'b': {
+    address: '/ctrl',
+    name: 'b',
+    type: 'f'
+  },
+  'c': {
+    address: '/ctrl',
+    name: 'c',
+    type: 'f'
+  },
+  'd': {
+    address: '/ctrl',
+    name: 'd',
+    type: 'f'
+  },
+  'e': {
+    address: '/ctrl',
+    name: 'e',
+    type: 'f'
+  },
+  'f': {
+    address: '/ctrl',
+    name: 'f',
+    type: 'f'
+  },
+  'g': {
+    address: '/ctrl',
+    name: 'g',
+    type: 'f'
+  },
+  'h': {
+    address: '/ctrl',
+    name: 'h',
+    type: 'f'
+  },
+  'i': {
+    address: '/ctrl',
+    name: 'i',
+    type: 'f'
+  },
+  'j': {
+    address: '/ctrl',
+    name: 'j',
+    type: 'f'
+  },
+  'k': {
+    address: '/ctrl',
+    name: 'k',
+    type: 'f'
+  },
+  'l': {
+    address: '/ctrl',
+    name: 'l',
+    type: 'f'
+  },
+  'm': {
+    address: '/ctrl',
+    name: 'm',
+    type: 'f'
+  },
+  'n': {
+    address: '/ctrl',
+    name: 'n',
+    type: 'f'
+  },
+  'o': {
+    address: '/ctrl',
+    name: 'o',
+    type: 'f'
+  },
+  'p': {
+    address: '/ctrl',
+    name: 'p',
+    type: 'f'
+  },
+  'q': {
+    address: '/ctrl',
+    name: 'q',
+    type: 'f'
+  },
+  'r': {
+    address: '/ctrl',
+    name: 'r',
+    type: 'f'
+  },
+  's': {
+    address: '/ctrl',
+    name: 's',
+    type: 'f'
+  },
+  't': {
+    address: '/ctrl',
+    name: 't',
+    type: 'f'
+  },
+  'u': {
+    address: '/ctrl',
+    name: 'u',
+    type: 'f'
+  },
+  'v': {
+    address: '/ctrl',
+    name: 'v',
+    type: 'f'
+  },
+  'x': {
+    address: '/ctrl',
+    name: 'x',
+    type: 'f'
+  },
+  'y': {
+    address: '/ctrl',
+    name: 'y',
+    type: 'f'
+  },
+  'z': {
+    address: '/ctrl',
+    name: 'z',
+    type: 'f'
+  }
 }
+
