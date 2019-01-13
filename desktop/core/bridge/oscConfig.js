af904aae01a5927cb1f67b3d75cc93b004fefe10
makio135 <radisson@MAC22219-6.local>
2019-01-13 01:22:24 +0100

    create a generic oscConfig instead of multiple files

---
+++ desktop/core/bridge/oscConfig.js
@@ -0,0 +1,29 @@
+'use strict'
+
+module.exports = {
+  name: 'Orca OSC',
+  protocol: 'osc',
+
+  defs: {
+    'A': {
+      address: '127.0.0.1',
+      port: 12000,
+      path: '/ctrlA',
+      pattern: 'sf'
+    },
+    'a': {
+      address: '127.0.0.1',
+      port: 12001,
+      path: '/ctrla',
+      name: 'a',
+      pattern: 's'
+    },
+    '0': {
+      address: '127.0.0.1',
+      port: 6010,
+      path: '/ctrl0',
+      name: '0',
+      pattern: 'i'
+    }
+  }
+}
