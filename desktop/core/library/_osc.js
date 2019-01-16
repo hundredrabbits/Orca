8b6ee0e7d61047c611cd3b2b697fa929a99ce4ea
Devine Lu Linvega <aliceffekt@gmail.com>
2019-01-17 09:48:00 +1200

    Sending OSC path/msg

--- desktop/core/library/_osc.js
+++ desktop/core/library/_osc.js
@@ -8,11 +8,13 @@ function OperatorOsc (orca, x, y, passive) {
   this.name = 'osc'
   this.info = 'Sends a OSC message.'
 
-  this.ports.haste.len = { x: -1, y: 0 }
+  this.ports.haste.pathlen = { x: -2, y: 0 }
+  this.ports.haste.msglen = { x: -1, y: 0 }
 
   this.haste = function () {
-    this.len = clamp(this.listen(this.ports.haste.len, true), 1, 16)
-    for (let x = 1; x <= this.len; x++) {
+    this.pathlen = clamp(this.listen(this.ports.haste.pathlen, true), 0, 16)
+    this.msglen = clamp(this.listen(this.ports.haste.msglen, true), 0, 16)
+    for (let x = 1; x <= this.pathlen + this.msglen; x++) {
       orca.lock(this.x + x, this.y)
     }
   }
@@ -22,12 +24,17 @@ function OperatorOsc (orca, x, y, passive) {
 
     this.draw = false
 
+    let path = ''
+    for (let x = 0; x < this.pathlen; x++) {
+      path += orca.glyphAt(1 + this.x + x, this.y)
+    }
+
     let msg = ''
-    for (let x = 0; x < this.len; x++) {
+    for (let x = this.pathlen; x < this.pathlen + this.msglen; x++) {
       msg += orca.glyphAt(1 + this.x + x, this.y)
     }
 
-    terminal.io.osc.send(msg)
+    terminal.io.osc.send(path, msg)
   }
 
   function clamp (v, min, max) { return v < min ? min : v > max ? max : v }
--- desktop/core/library/_udp.js
+++ desktop/core/library/_udp.js
@@ -11,7 +11,7 @@ function OperatorUdp (orca, x, y, passive) {
   this.ports.haste.len = { x: -1, y: 0 }
 
   this.haste = function () {
-    this.len = clamp(this.listen(this.ports.haste.len, true), 1, 16)
+    this.len = clamp(this.listen(this.ports.haste.len, true), 0, 16)
     for (let x = 1; x <= this.len; x++) {
       orca.lock(this.x + x, this.y)
     }
