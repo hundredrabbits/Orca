07bb0b3018452ff58dda2fd7e192e3baf16931e6
Devine Lu Linvega <aliceffekt@gmail.com>
2019-02-21 15:13:44 +0900

    Improved examples

--- desktop/main.js
+++ desktop/main.js
@@ -14,8 +14,8 @@ app.on('ready', () => {
     minWidth: 320,
     minHeight: 320,
     backgroundColor: '#000',
-    resizable: true,
     icon: __dirname + '/' + { darwin: 'icon.icns', linux: 'icon.png', win32: 'icon.ico' }[process.platform] || 'icon.ico',
+    resizable: true,
     frame: process.platform !== 'darwin',
     skipTaskbar: process.platform === 'darwin',
     autoHideMenuBar: process.platform === 'darwin'
