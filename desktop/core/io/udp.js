a6262a72aa4fb6f83fd97154bab05865037486e5
Devine Lu Linvega <aliceffekt@gmail.com>
2019-04-11 10:37:20 +0900

    Fixed UDP bpm change..

--- desktop/core/io/udp.js
+++ desktop/core/io/udp.js
@@ -81,7 +81,7 @@ function Udp (terminal) {
     } else if (key === 'f' && Number.isInteger(int)) {
       terminal.orca.f = int
     } else if (key === 'b' && Number.isInteger(int)) {
-      terminal.setSpeed(int)
+      terminal.clock.setSpeed(int, true)
     } else if (key === 'w' && val.length >= 4 && val.indexOf(':') > -1) {
       const pos = val.substr(1).split(':')
       terminal.orca.write(parseInt(pos[0]), parseInt(pos[1]), val.substr(0, 1))
