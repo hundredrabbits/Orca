a247a8d43090a5f9484e20eea6b37adce3601aa3
Devine Lu Linvega <aliceffekt@gmail.com>
2019-04-17 12:50:05 +0900

    removed bpm lock

--- desktop/sources/scripts/clock.js
+++ desktop/sources/scripts/clock.js
@@ -69,10 +69,8 @@ function Clock (terminal) {
     }
     if (this.intervals.length === 8) {
       const sum = this.intervals.reduce((sum, interval) => { return sum + interval })
-      const bpm = parseInt((1000 / sum) * 60)
-      if (Math.abs(bpm - this.speed.target) > 1) {
-        this.set(null, bpm)
-      }
+      const bpm = Math.floor((1000 / sum) * 60)
+      this.set(null, bpm)
     }
 
     const now = performance.now()
