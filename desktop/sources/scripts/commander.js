99d016da22e9d508191647a5e1024574caf27879
Yancy Way <yancyway@gmail.com>
2019-04-21 20:57:45 -0700

    handling for caps lock on windows

--- desktop/sources/scripts/commander.js
+++ desktop/sources/scripts/commander.js
@@ -114,10 +114,10 @@ function Commander (terminal) {
 
     if (event.keyCode === 191 && (event.metaKey || event.ctrlKey)) { terminal.cursor.comment(); event.preventDefault(); return }
 
-    if (event.key === 'c' && (event.metaKey || event.ctrlKey)) { terminal.cursor.copy(); event.preventDefault(); return }
-    if (event.key === 'x' && (event.metaKey || event.ctrlKey)) { terminal.cursor.cut(); event.preventDefault(); return }
-    if (event.key === 'v' && (event.metaKey || event.ctrlKey)) { terminal.cursor.paste(); event.preventDefault(); return }
-    if (event.key === 'a' && (event.metaKey || event.ctrlKey)) { terminal.cursor.selectAll(); event.preventDefault(); return }
+    if ((event.key === 'c' || event.key === 'C') && (event.metaKey || event.ctrlKey)) { terminal.cursor.copy(); event.preventDefault(); return }
+    if ((event.key === 'x' || event.key === 'X') && (event.metaKey || event.ctrlKey)) { terminal.cursor.cut(); event.preventDefault(); return }
+    if ((event.key === 'v' || event.key === 'V') && (event.metaKey || event.ctrlKey)) { terminal.cursor.paste(); event.preventDefault(); return }
+    if ((event.key === 'a' || event.key === 'A') && (event.metaKey || event.ctrlKey)) { terminal.cursor.selectAll(); event.preventDefault(); return }
 
     // Undo/Redo
     if (event.key === 'z' && (event.metaKey || event.ctrlKey) && event.shiftKey) { terminal.history.redo(); event.preventDefault(); return }
