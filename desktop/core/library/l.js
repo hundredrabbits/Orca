7862d32aecd3ea289ecd6a882a0eee336697de4d
Devine Lu Linvega <aliceffekt@gmail.com>
2019-04-23 16:40:50 +0900

    L with rate

--- desktop/core/library/l.js
+++ desktop/core/library/l.js
@@ -9,21 +9,25 @@ function OperatorL (orca, x, y, passive) {
   this.info = 'Loops a number of eastward operators.'
 
   this.ports.haste.len = { x: -1, y: 0 }
+  this.ports.haste.rate = { x: -2, y: 0 }
 
   this.haste = function () {
-    this.len = this.listen(this.ports.haste.len, true)
-    for (let x = 1; x <= this.len; x++) {
+    const len = this.listen(this.ports.haste.len, true)
+    for (let x = 1; x <= len; x++) {
       orca.lock(this.x + x, this.y)
     }
   }
 
   this.run = function () {
-    this.len = this.listen(this.ports.haste.len, true)
+    const len = this.listen(this.ports.haste.len, true)
+    const rate = this.listen(this.ports.haste.rate, true)
     const a = []
-    for (let x = 1; x <= this.len; x++) {
+    for (let x = 1; x <= len; x++) {
       a.push(orca.glyphAt(this.x + x, this.y))
     }
-    a.push(a.shift())
+    for (let x = 1; x <= rate; x++) {
+      a.push(a.shift())
+    }
     for (const id in a) {
       const x = parseInt(id) + 1
       orca.write(this.x + x, this.y, a[id])
