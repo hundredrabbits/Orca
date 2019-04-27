0844223ae2d947805798b211d24d6df299e2fdda
Devine Lu Linvega <aliceffekt@gmail.com>
2019-04-27 09:08:39 +0900

    New increment operator

--- desktop/core/library/i.js
+++ desktop/core/library/i.js
@@ -8,20 +8,16 @@ function OperatorI (orca, x, y, passive) {
   this.name = 'increment'
   this.info = 'Increments southward operator.'
 
-  this.ports.haste.min = { x: -1, y: 0 }
-  this.ports.input.max = { x: 1, y: 0 }
+  this.ports.haste.rate = { x: -1, y: 0 }
+  this.ports.input.mod = { x: 1, y: 0 }
   this.ports.output = { x: 0, y: 1 }
 
   this.run = function () {
-    const min = this.listen(this.ports.haste.min, true, 0, 36, 0)
-    const max = this.listen(this.ports.input.max, true, 0, 36, 36)
+    const rate = this.listen(this.ports.haste.rate, true, 0, 36, 1)
+    const mod = this.listen(this.ports.input.mod, true, 0, 36, 36)
     const val = this.listen(this.ports.output, true)
-    if (min === max) { return }
-    const next = val + (min < max ? 1 : -1)
-    const _min = min < max ? min : max
-    const _max = min > max ? min : max
-    const res = next >= _max ? _min : next < _min ? _max - 1 : next
-    this.output(`${orca.keyOf(res)}`, false, true)
+    const res = orca.keyOf((val + rate) % mod)
+    this.output(`${res}`, false, true)
   }
 }
 
