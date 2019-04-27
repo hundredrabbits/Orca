c27558955439ac9d8fb1ef19d4659d4aa2d4672f
Devine Lu Linvega <aliceffekt@gmail.com>
2019-04-27 09:25:03 +0900

    Renamed rate ports to step

--- desktop/core/library/i.js
+++ desktop/core/library/i.js
@@ -8,15 +8,15 @@ function OperatorI (orca, x, y, passive) {
   this.name = 'increment'
   this.info = 'Increments southward operator.'
 
-  this.ports.haste.rate = { x: -1, y: 0 }
+  this.ports.haste.step = { x: -1, y: 0 }
   this.ports.input.mod = { x: 1, y: 0 }
   this.ports.output = { x: 0, y: 1 }
 
   this.run = function () {
-    const rate = this.listen(this.ports.haste.rate, true, 0, 36, 1)
+    const step = this.listen(this.ports.haste.step, true, 0, 36, 1)
     const mod = this.listen(this.ports.input.mod, true, 0, 36, 36)
     const val = this.listen(this.ports.output, true)
-    const res = orca.keyOf((val + rate) % mod)
+    const res = orca.keyOf((val + step) % mod)
     this.output(`${res}`, false, true)
   }
 }
--- desktop/core/library/l.js
+++ desktop/core/library/l.js
@@ -9,7 +9,7 @@ function OperatorL (orca, x, y, passive) {
   this.info = 'Loops a number of eastward operators.'
 
   this.ports.haste.len = { x: -1, y: 0 }
-  this.ports.haste.rate = { x: -2, y: 0 }
+  this.ports.haste.step = { x: -2, y: 0 }
 
   this.haste = function () {
     const len = this.listen(this.ports.haste.len, true)
@@ -20,12 +20,12 @@ function OperatorL (orca, x, y, passive) {
 
   this.run = function () {
     const len = this.listen(this.ports.haste.len, true)
-    const rate = this.listen(this.ports.haste.rate, true)
+    const step = this.listen(this.ports.haste.step, true)
     const a = []
     for (let x = 1; x <= len; x++) {
       a.push(orca.glyphAt(this.x + x, this.y))
     }
-    for (let x = 1; x <= rate; x++) {
+    for (let x = 1; x <= step; x++) {
       a.push(a.shift())
     }
     for (const id in a) {
