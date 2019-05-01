4a03dce0b7536ef32da4b125d4a510cd48b64cc3
Devine Lu Linvega <aliceffekt@gmail.com>
2019-05-01 11:32:31 +0900

    versioning

--- desktop/sources/scripts/terminal.js
+++ desktop/sources/scripts/terminal.js
@@ -11,7 +11,7 @@ function Terminal () {
   const Theme = require('./lib/theme')
   const Controller = require('./lib/controller')
 
-  this.version = 101
+  this.version = 102
   this.library = require('../../core/library')
 
   this.orca = new Orca()
