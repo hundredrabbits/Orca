70f74677d9c609fbdd00af8e7b5d61966c7831de
bcgreen24 <bcgreen@gmail.com>
2019-05-01 21:24:07 -0500

    Update j.js

--- desktop/core/library/j.js
+++ desktop/core/library/j.js
@@ -6,17 +6,17 @@ function OperatorJ (orca, x, y, passive) {
   Operator.call(this, orca, x, y, 'j', passive)
 
   this.name = 'jumper'
-  this.info = 'Outputs the northward operator.'
+  this.info = 'Copies the northward value southward.'
 
-  this.ports.haste.val = { x: 0, y: -1 }
-  this.ports.output = { x: 0, y: 1 }
 
-  this.haste = function () {
-    orca.lock(this.x, this.y + 1)
-  }
+  this.ports.haste.y = { x: -1, y: 0 }
+  this.ports.input.val = { x: 0, y: -1 }
 
   this.operation = function (force = false) {
-    return this.listen(this.ports.haste.val)
+    const y = this.listen(this.ports.haste.y, true) + 1
+
+    this.ports.output = { x: 0, y: y }
+    return this.listen(this.ports.input.val)
   }
 }
 
