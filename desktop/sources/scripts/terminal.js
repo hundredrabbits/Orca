cbbf3df4a05b344171eb31ca8700247e5282b026
Devine Lu Linvega <aliceffekt@gmail.com>
2019-05-10 09:49:29 +0900

    version

--- desktop/sources/scripts/terminal.js
+++ desktop/sources/scripts/terminal.js
@@ -13,7 +13,7 @@ import Controller from './lib/controller.js'
 import library from '../../core/library.js'
 
 export default function Terminal () {
-  this.version = 118
+  this.version = 119
   this.library = library
 
   this.orca = new Orca(this)
