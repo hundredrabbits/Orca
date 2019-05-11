28f8eed451062d7510347bb58de658c985855acf
Devine Lu Linvega <aliceffekt@gmail.com>
2019-05-12 07:39:48 +0900

    Better U from Npisanti

--- desktop/core/library/u.js
+++ desktop/core/library/u.js
@@ -8,31 +8,14 @@ export default function OperatorU (orca, x, y, passive) {
   this.name = 'Uclid'
   this.info = 'Bangs based on the Euclidean pattern.'
 
-  this.ports.haste.step = { x: -1, y: 0, clamp: { min: 1 } }
+  this.ports.haste.step = { x: -1, y: 0, clamp: { min: 0 }, default: '1' }
   this.ports.input.max = { x: 1, y: 0, clamp: { min: 1 }, default: '8' }
   this.ports.output = { x: 0, y: 1, bang: true }
 
   this.operation = function (force = false) {
     const step = this.listen(this.ports.haste.step, true)
     const max = this.listen(this.ports.input.max, true)
-    let segs = []
-    for (let i = 0; i < max; i++) {
-      segs.push([i < step ? 1 : 0])
-    }
-    let l = 0
-    while (l = segs.length - 1) {
-      let from = 0
-      while (from < l && segs[0].join('') === segs[from].join('')) { from++ }
-      if (from === l) { break }
-      let to = l
-      while (to > 0 && segs[l].join('') === segs[to].join('')) { to-- }
-      if (to === 0) { break }
-      const count = Math.min(from, l - to)
-      segs = segs.slice(0, count).map((group, i) => {
-        return group.concat(segs[l - i])
-      }).concat(segs.slice(count, -count))
-    }
-    const sequence = [].concat.apply([], segs)
-    return sequence[orca.f % sequence.length] === 1
+    const bucket = (step * (orca.f + max - 1)) % max + step
+    return bucket >= max
   }
 }
