d63e563066abaa3d26ddd3f90aca11b40be01fb9
Devine Lu Linvega <aliceffekt@gmail.com>
2019-06-03 13:35:13 +0900

    Fixed issue with UDP

--- desktop/core/library/_udp.js
+++ desktop/core/library/_udp.js
@@ -9,13 +9,14 @@ export default function OperatorUdp (orca, x, y, passive) {
   this.info = 'Sends UDP message'
 
   this.operation = function (force = false) {
-    if (!this.hasNeighbor('*') && force === false) { return }
 
     for (let x = 1; x <= 36; x++) {
       const g = orca.glyphAt(this.x + x, this.y)
       if (g === '.') { break }
       orca.lock(this.x + x, this.y)
     }
+
+    if (!this.hasNeighbor('*') && force === false) { return }
 
     let msg = ''
     for (let x = 1; x <= 36; x++) {
