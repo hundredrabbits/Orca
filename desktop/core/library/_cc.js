0af2d29552eedd4f8eadb45662de682eccb95630
okyeron <1909661+okyeron@users.noreply.github.com>
2019-07-01 22:08:00 -0500

    re-arranged parameters

--- desktop/core/library/_cc.js
+++ desktop/core/library/_cc.js
@@ -7,18 +7,18 @@ export default function OperatorCC (orca, x, y) {
 
   this.name = 'cc'
   this.info = 'Sends MIDI cc/program change/pitchbend'
-  this.ports.message = { x: 1, y: 0, clamp: { min: 0, max: 15 } }
-  this.ports.channel = { x: 2, y: 0, clamp: { min: 0, max: 15 } }
-  this.ports.data1 = { x: 3, y: 0, clamp: { min: 0 } }
-  this.ports.data2 = { x: 4, y: 0, clamp: { min: 0 } }
+  this.ports.channel = { x: 1, y: 0, clamp: { min: 0, max: 15 } }
+  this.ports.data1 = { x: 2, y: 0, clamp: { min: 0 } }
+  this.ports.data2 = { x: 3, y: 0, clamp: { min: 0 } }
+  this.ports.message = { x: 4, y: 0, clamp: { min: 0, max: 15 } }
 
   this.operation = function (force = false) {
     if (!this.hasNeighbor('*') && force === false) { return }
-    if (this.listen(this.ports.message) === '.') { return }
     if (this.listen(this.ports.channel) === '.') { return }
     if (this.listen(this.ports.data1) === '.') { return }
+    if (this.listen(this.ports.message) === '.') { return }
 
-    const message = this.listen(this.ports.message, true)
+	const message = this.listen(this.ports.message, true)
     const channel = this.listen(this.ports.channel, true)
 	const data1 = this.listen(this.ports.data1, true)
 	
