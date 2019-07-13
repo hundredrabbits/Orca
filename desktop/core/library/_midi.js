f37d9d9a5977347b1623888b046c01416075b9d4
Devine Lu Linvega <aliceffekt@gmail.com>
2019-07-13 10:15:55 +0900

    Don't add defaults to midi operators

--- desktop/core/library/_midi.js
+++ desktop/core/library/_midi.js
@@ -8,14 +8,16 @@ export default function OperatorMidi (orca, x, y, passive) {
   this.name = 'midi'
   this.info = 'Sends MIDI note'
   this.ports.channel = { x: 1, y: 0, clamp: { min: 0, max: 16 } }
-  this.ports.octave = { x: 2, y: 0, default: '3', clamp: { min: 0, max: 8 } }
-  this.ports.note = { x: 3, y: 0, default: 'C' }
+  this.ports.octave = { x: 2, y: 0, clamp: { min: 0, max: 8 } }
+  this.ports.note = { x: 3, y: 0 }
   this.ports.velocity = { x: 4, y: 0, default: 'f', clamp: { min: 0, max: 16 } }
   this.ports.length = { x: 5, y: 0, default: '1', clamp: { min: 0, max: 16 } }
 
   this.operation = function (force = false) {
     if (!this.hasNeighbor('*') && force === false) { return }
     if (this.listen(this.ports.channel) === '.') { return }
+    if (this.listen(this.ports.octave) === '.') { return }
+    if (this.listen(this.ports.note) === '.') { return }
     if (!isNaN(this.listen(this.ports.note))) { return }
 
     const channel = this.listen(this.ports.channel, true)
