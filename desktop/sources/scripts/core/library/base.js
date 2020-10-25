3cc6a1fedd0c83f8059c94b68662d5ea05083b73
unthingable <unthingable@users.noreply.github.com>
2020-10-26 00:28:02 +0100

    add comments to base lang

--- desktop/sources/scripts/core/library/base.js
+++ desktop/sources/scripts/core/library/base.js
@@ -5,6 +5,22 @@
 
 library.base = {}
 
+library.base['#'] = function OperatorComment (orca, x, y, passive) {
+    Operator.call(this, orca, x, y, '#', true)
+
+    this.name = 'comment'
+    this.info = 'Halts line'
+    this.draw = false
+
+    this.operation = function () {
+      for (let x = this.x + 1; x <= orca.w; x++) {
+        orca.lock(x, this.y)
+        if (orca.glyphAt(x, this.y) === this.glyph) { break }
+      }
+      orca.lock(this.x, this.y)
+    }
+}
+
 for (let i = 0; i <= 9; i++) {
     library.base[`${i}`] = function OperatorNull (orca, x, y, passive) {
       Operator.call(this, orca, x, y, '.', false)
