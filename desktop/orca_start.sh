299aa2e7e64da44dc7f44995d8cd91ae785acd0f
smonff <seb@feugere.net>
2023-03-24 17:50:10 +0100

    Add shebang in the script

--- desktop/orca_start.sh
+++ desktop/orca_start.sh
@@ -1,7 +1,10 @@
+#!/usr/bin/env bash
+
 ORCA_HOME=/home/smonff/Orca-linux-arm64
 ORCA_BIN=Orca
 
 cd ${ORCA_HOME}
 
+# Would crash on ARM64 ships with the sandbox enabled
 ./${ORCA_BIN} --no-sandbox 
 
