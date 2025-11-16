7fe2efaff9565d5ed45e74754fb3f30a684e9a77
Devie Lu Linvega <aliceffekt@gmail.com>
2025-11-16 10:15:28 -0800

    Increment by 0 should not increment output

--- desktop/sources/scripts/core/library.js
+++ desktop/sources/scripts/core/library.js
@@ -150,7 +150,7 @@ library.i = function OperatorI (orca, x, y, passive) {
   this.name = 'increment'
   this.info = 'Increments southward operand'
 
-  this.ports.step = { x: -1, y: 0, default: '1' }
+  this.ports.step = { x: -1, y: 0 }
   this.ports.mod = { x: 1, y: 0 }
   this.ports.output = { x: 0, y: 1, sensitive: true, reader: true, output: true }
 
@@ -158,7 +158,7 @@ library.i = function OperatorI (orca, x, y, passive) {
     const step = this.listen(this.ports.step, true)
     const mod = this.listen(this.ports.mod, true)
     const val = this.listen(this.ports.output, true)
-    return orca.keyOf((val + step) % (mod > 0 ? mod : 36))
+    return mod ? orca.keyOf((val + step) % mod) : '0'
   }
 }
 
